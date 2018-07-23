d162a2028c896f468919b475e5ae2e6639c2c00f
eamoses <e.a.moses@gmail.com>
2018-07-22 19:03:37 -0500

    Placeholder data added to functioning home site

---
+++ scripts/onload.js
@@ -0,0 +1,13 @@
+window.onload = ()=>{
+    let setPage = window.location.hash
+    setPage = setPage.split('#')[1]
+    if (setPage) _page(setPage)
+    _set_year_to_footer(document.getElementById('my-custom-footer'))
+  }
+  
+  _set_year_to_footer = (footer)=>{
+    const year = new Date
+    footer.innerHTML = /*html*/`
+      <div> &copy; ${year.getFullYear()} <a href="https://github.com/eamoses" target="_blank"> Emily Anne Moses </a> </div>
+    `
+  }  
