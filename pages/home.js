945be575fc58725cf51347414dba58c42172ce4a
eamoses <e.a.moses@gmail.com>
2018-08-04 20:17:43 -0500

    altering sheets url

--- pages/home.js
+++ pages/home.js
@@ -1,4 +1,4 @@
-var foodData = "https://spreadsheets.google.com/feeds/list/1za1lKiDfcE4lCrkXzSpN970IOOB-dHsXFAIjF5gn_Ms/od6/public/values?alt=json"
+var foodData = "https://spreadsheets.google.com/feeds/list/1za1lKiDfcE4lCrkXzSpN970IOOB-dHsXFAIjF5gn_Ms/1/public/values?alt=json"
 
 _GET = (url, callback)=>{
   var xhr = new XMLHttpRequest();
