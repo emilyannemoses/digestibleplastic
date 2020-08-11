f25689af296d734b6f5183ec2f3d81283ccf0377
eamoses <e.a.moses@gmail.com>
2020-08-11 11:21:09 -0500

    adding new card style

--- pages/recipes.js
+++ pages/recipes.js
@@ -1,5 +1,4 @@
 var recipesData = "https://spreadsheets.google.com/feeds/list/1za1lKiDfcE4lCrkXzSpN970IOOB-dHsXFAIjF5gn_Ms/1/public/values?alt=json"
-var data;
 
 _GET = (url, callback)=>{
   var xhr = new XMLHttpRequest();
@@ -12,43 +11,31 @@ _GET = (url, callback)=>{
   xhr.send(null)
 }
 
-function toggle(data, id) {
-  var newData = JSON.parse(data[id]);
-  console.log(newData, id)
-}
-// toggle = (data, id)=>{
-//   console.log(data, id)
-  // const column = document.getElementById("columns")
-  // const recipe = document.getElementById("recipe")
-  // recipe.style.display = 'block'
-  // column.style.display = 'none'
-
-// }
-
 _GET(recipesData, (data)=>{
   data = data.feed.entry
   const column = document.getElementById("columns")
   const recipe = document.getElementById("recipe")
   for (const row of data) {
-    console.log(data)
     column.innerHTML += `
-      <div onclick='toggle("${data[row.gsx$id.$t]}", "${row.gsx$id.$t}")' class="figure terms">
-        <div style="font-size:32px;width:75vw;text-align:center;">
-          <span style="float:left;">${row.gsx$mealdescription.$t}</span>
-          <span style="margin:auto;">•</span>
-          <span style="float:right;">${row.gsx$arabicdescription.$t}</span>
-          <br />
-        </div>
-        <div style="float:left;">
-          <img src="${row.gsx$imageurl1.$t}"/>
-          <img src="${row.gsx$imageurl2.$t}"/>
-          <img src="${row.gsx$imageurl3.$t}"/>
-        </div>
-        <div style="float:right;">
-          ${row.gsx$ingredients.$t}
-          <span style="opacity:0;">${row.gsx$tags.$t}</span>
-          <br />
-          <a>Recipe</a>
+      <div class="figure">
+        <div class="terms">
+          <div class="text">
+            <span style="font-size:26px;"">${row.gsx$mealdescription.$t} • ${row.gsx$arabicdescription.$t}</span>
+            <br />
+          </div>
+          <div class="text">
+            <img src="${row.gsx$imageurl1.$t}"/>
+            <img src="${row.gsx$imageurl2.$t}"/>
+            <img src="${row.gsx$imageurl3.$t}"/>
+          </div>
+          <div class="text">
+            ${row.gsx$ingredients.$t}
+            <span style="opacity:0;">${row.gsx$tags.$t}</span>
+            <br />
+            <br />
+            ${row.gsx$recipe.$t}
+            <br />
+          </div>
         </div>
       </div>
     `
