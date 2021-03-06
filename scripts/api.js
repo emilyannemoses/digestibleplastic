0e557eabce71d2f1ebbb07a6848e51b083e66add
Emily Anne Moses <e.a.moses@gmail.com>
2021-03-06 12:38:34 -0600

    update

--- scripts/api.js
+++ scripts/api.js
@@ -1,4 +1,4 @@
-var foodData = "https://spreadsheets.google.com/feeds/list/1za1lKiDfcE4lCrkXzSpN970IOOB-dHsXFAIjF5gn_Ms/od6/public/values?alt=json"
+var foodData = "https://spreadsheets.google.com/feeds/list/2PACX-1vQ-QvEqrAuHM1GXGHPuy9sUGcB9C4O9tlcIAE_YuzugkWixCTP9Fz0i5RKMn203cQItCkn3FLOMaf3E/od6/public/values?alt=json"
 
 _GET = (url, callback)=>{
   var xhr = new XMLHttpRequest();
